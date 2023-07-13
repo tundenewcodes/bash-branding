56c5f452aeafc554f12c930dd708b03c911cb15a
tundenewcodes <tundef@eraconneckt.com>
2023-07-13 17:43:01 +0100

    fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,22 +4,18 @@ import Work from "../components/Work";
 import Bio from "../components/bio";
 import Contact from "../components/contact/Contact";
 import Projects from "../components/projects/Projects";
-import usePageSetup from "../hooks/usePageSetup";
 
 
-const Home = ({allWorkData}) => {
+const Home = ({ allWorkData }) => {
  
-  const { selectedPage, setSelectedPage, isTopOfPage } = usePageSetup();
   return (
     <>
-      <Bio isTopOfPage={isTopOfPage}
-        selectedPage={selectedPage}
-        setSelectedPage={setSelectedPage}/>
-      <Work setSelectedPage={setSelectedPage} allWorkData={allWorkData}/>
-      <Projects setSelectedPage={setSelectedPage}/>
-      <Communities setSelectedPage={setSelectedPage}/>
-      <Podcast  setSelectedPage={setSelectedPage} />
-      <Contact setSelectedPage={setSelectedPage} />
+      <Bio />
+      <Work allWorkData={allWorkData} />
+      <Projects />
+      <Communities />
+      <Podcast />
+      <Contact />
     </>
   );
 };
